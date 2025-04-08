05a6d02f0578691258da7d9f54a64e16b4286fd9
Aldair Gutierrez <aldairgguer@gmail.com>
2025-04-08 11:14:12 -0500

    feat: agregar Swagger para documentación de API

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import app from "./app";
-import { PrismaClient } from "@prisma/client";
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const PORT = process.env.PORT || 3000;
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor ejecutándose en http://localhost:${PORT}`);
