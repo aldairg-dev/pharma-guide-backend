afaeff55292d6d6046fb531fb15f63970fda4864
Aldair Gutierrez <aldairgguer@gmail.com>
2025-04-12 15:09:26 -0500

    feat: eliminar migraciones obsoletas y actualizar esquema de base de datos con nuevos modelos y relaciones

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,24 +4,66 @@ import bcrypt from "bcrypt";
 const prisma = new PrismaClient();
 
 async function main() {
-  const hashedPassword = await bcrypt.hash("pharmaGuide", 10);
+  try {
+    // -> Crear un nuevo status
+    const existingStatus = await prisma.status.findUnique({
+      where: { name: "active" },
+    });
 
-  await prisma.user.create({
-    data: {
-      full_name: "Test pharmaGuide",
-      email: "test@pharma.guide",
-      password: hashedPassword,
-    },
-  });
+    if (!existingStatus) {
+      await prisma.status.create({
+        data: {
+          name: "active",
+        },
+      });
+      console.log("Seed de status registrado con éxito!");
+    } else {
+      console.log("El status ya existe.");
+    }
 
-  console.log("Seed de usuario registrado con exito!");
-}
+    // -> Crear un nuevo rol
+    const existingRole = await prisma.role.findUnique({
+      where: { name: "admin" },
+    });
 
-main()
-  .catch((e) => {
+    if (!existingRole) {
+      await prisma.role.create({
+        data: {
+          name: "admin",
+        },
+      });
+      console.log("Seed de rol registrado con éxito!");
+    } else {
+      console.log("El rol ya existe.");
+    }
+
+    // -> Crear un nuevo usuario
+    const hashedPassword = await bcrypt.hash("pharmaGuide", 10);
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email: "test@pharma.guide" },
+    });
+
+    if (!existingUser) {
+      await prisma.user.create({
+        data: {
+          full_name: "Test pharmaGuide",
+          email: "test@pharma.guide",
+          password: hashedPassword,
+          roleId: 1,
+          statusId: 1,
+        },
+      });
+      console.log("Seed de usuario registrado con éxito!");
+    } else {
+      console.log("El usuario ya existe.");
+    }
+  } catch (e) {
     console.error("Error ejecutando seed:", e);
     process.exit(1);
-  })
-  .finally(async () => {
+  } finally {
     await prisma.$disconnect();
-  });
+  }
+}
+
+main();
