f7b057e963ce751218e75ced8b2a60ff3556d29d
Aldair Gutierrez <aldairgguer@gmail.com>
2025-04-28 23:06:52 -0500

    feat: implementation userService

--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -1,44 +1,64 @@
 import { PrismaClient, User } from "@prisma/client";
-
 const prisma = new PrismaClient();
 
 export class UserService {
-
   async getUser(): Promise<User[]> {
     try {
-      const users = await prisma.user.findMany({
-        where: {
-          isDeleted: false,
-        },
+      return await prisma.user.findMany({
+        where: { isDeleted: false },
       });
-      return users;
     } catch (error) {
       console.error("Error fetching users:", error);
       throw new Error("An error occurred while fetching users.");
     }
   }
-  
 
-  async deleteUser(idUser: number): Promise<User | null> {
+  async getUserById(idUser: number): Promise<User | null> {
     try {
-      const userData = await prisma.user.findUnique({
-        where: {
-          id: idUser,
-          isDeleted: false,
-        },
+      return await prisma.user.findFirst({
+        where: { id: idUser, isDeleted: false },
+      });
+    } catch (error) {
+      console.error("Error fetching user:", error);
+      throw new Error("An error occurred while fetching the user.");
+    }
+  }
+
+  async updateUser(user: User): Promise<User | null> {
+    try {
+      const existingUser = await prisma.user.findFirst({
+        where: { id: user.id, isDeleted: false },
       });
 
-      if (!userData || userData.isDeleted) {
+      if (!existingUser) {
         throw new Error("User not found or already deleted.");
       }
 
       const updatedUser = await prisma.user.update({
-        where: {
-          id: userData.id,
-        },
-        data: {
-          isDeleted: true,
-        },
+        where: { id: user.id },
+        data: user,
+      });
+
+      return updatedUser;
+    } catch (error) {
+      console.error("Error updating user:", error);
+      throw new Error("An error occurred while updating the user.");
+    }
+  }
+
+  async deleteUser(idUser: number): Promise<User | null> {
+    try {
+      const userData = await prisma.user.findFirst({
+        where: { id: idUser, isDeleted: false },
+      });
+
+      if (!userData) {
+        throw new Error("User not found or already deleted.");
+      }
+
+      const updatedUser = await prisma.user.update({
+        where: { id: userData.id },
+        data: { isDeleted: true },
       });
 
       return updatedUser;
