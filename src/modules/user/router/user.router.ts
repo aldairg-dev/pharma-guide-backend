1b1666b36f0f1de92d2d08a1293607276bd5f485
Aldair Gutierrez <aldairgguer@gmail.com>
2025-04-28 23:11:46 -0500

    feat: agregar rutas para obtener y actualizar usuarios en el router de usuarios

--- src/modules/user/router/user.router.ts
+++ src/modules/user/router/user.router.ts
@@ -4,6 +4,13 @@ import { UserController } from "../controller/user.controller";
 const routerUser = Router();
 const userController = new UserController();
 
+routerUser.get("/user", userController.getUser.bind(userController));
+routerUser.get(
+  "/user/:idUser",
+  userController.getUserById.bind(userController)
+);
+
+routerUser.put("/user/:idUser", userController.updateUser.bind(userController));
 routerUser.delete("/user", userController.deleteUser.bind(userController));
 
 export default routerUser;
