ed6c0e08ed3853f841553a8577f55f5528b18785
Aldair Gutierrez <aldairgguer@gmail.com>
2025-05-08 11:02:13 -0500

    feat: agregar soporte para variables de entorno usando cross-env en scripts y configurar dotenv en index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import app from "./app";
+import * as dotenv from 'dotenv';
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
@@ -6,6 +7,11 @@ const PORT = process.env.PORT || 3001;
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV || "development"}`,
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 Servidor ejecutándose en http://localhost:${PORT}`);
+  console.log(`🌱 Entorno: ${process.env.NODE_ENV}`);
 });
