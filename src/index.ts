eaf0e7b3711f7a96770fe44ed68e271f3fee14d0
Aldair Gutierrez <aldairgguer@gmail.com>
2025-05-09 01:10:53 -0500

    feat: configurar el puerto del servidor y mejorar la estructura de inicio

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./swagger.json";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
+
+const PORT = process.env.PORT || 8080;
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.use("/", (req, res) => {
+  res.send("Welcome to Pharma Guide API");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
