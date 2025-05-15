9f36c5422c8e24280acbac5d498515b40ca39566
Aldair Gutierrez <aldairgguer@gmail.com>
2025-05-14 23:58:20 -0500

    feat: add Drug model import and enhance createDrug method in DrugService

--- src/modules/Drug/service/drug.service.ts
+++ src/modules/Drug/service/drug.service.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/default";
+import { Drug, PrismaClient } from "@prisma/client/default";
 import { empty } from "@prisma/client/runtime/library";
 const prisma = new PrismaClient();
 
@@ -10,6 +10,29 @@ export class DrugService {
     } catch (error: any) {
       console.error("Error fetching drugs: ", error.message);
       throw new Error("An error occurred while fetching drugs.");
+    }
+  }
+
+  async createDrug(drug: Drug) {
+    try {
+      const existingDrug = await prisma.drug.findFirst({
+        where: {
+          userId: drug.userId,
+          isDeleted: false,
+        },
+      });
+
+      if (existingDrug) {
+        throw new Error("The drug already exists.");
+      } else {
+        const newDrug = await prisma.drug.create({
+          data: drug,
+        });
+        return newDrug;
+      }
+    } catch (error: any) {
+      console.error("Error creating drug: ", error.message);
+      throw new Error("An error occurred while creating the drug.");
     }
   }
 }
