f678d9e7c09a089e321640bf8c6b76005c53cfcd
Aldair Gutierrez <aldairgguer@gmail.com>
2025-06-02 15:33:49 -0500

    fix: add leading slashes to route paths in IaManagement router

--- src/modules/Ia-management/router/ia-management.router.ts
+++ src/modules/Ia-management/router/ia-management.router.ts
@@ -5,27 +5,27 @@ const routerManagementIA = Router();
 const managementIaController = new ManagementIaController();
 
 routerManagementIA.get(
-  "ia-management",
+  "/ia-management",
   managementIaController.getManegement.bind(managementIaController)
 );
 
 routerManagementIA.post(
-  "ia-management",
+  "/ia-management",
   managementIaController.createManegemet.bind(managementIaController)
 );
 
 routerManagementIA.get(
-  "ia-management/:id",
+  "/ia-management/:id",
   managementIaController.getOneManagement.bind(managementIaController)
 );
 
 routerManagementIA.put(
-  "ia-management/:id",
+  "/ia-management/:id",
   managementIaController.updateManagement.bind(managementIaController)
 );
 
 routerManagementIA.delete(
-  "ia-management/:id",
+  "/ia-management/:id",
   managementIaController.deleteManagement.bind(managementIaController)
 );
 
