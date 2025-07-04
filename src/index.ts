5b8210db49ea6186abdd606da8588f3c9539782e
Aldair Gutierrez <aldairgguer@gmail.com>
2025-07-04 16:46:25 -0500

    feat: add GitHub webhook endpoint to handle incoming events

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,12 @@ const PORT = process.env.PORT || 8080;
 app.get("/", (req, res) => {
   res.json({ message: "API funcionando correctamente desde cualquier origen" });
 });
+
+app.post("/webhooks/github", (req, res) => {
+  console.log("Evento de GitHub recibido:", req.body);
+  res.status(200).send("OK");
+});
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
